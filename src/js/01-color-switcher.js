782b4501252f724f83d2eb8f11274938aff5cd4f
shevchenkoAndriy <s.s.student.khai.edu>
2021-11-06 23:06:51 +0200

    edd feature color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,34 +1,24 @@
 buttonStartRef = document.querySelector('button[data-start]');
-console.log('🚀 ~ file: 01-color-switcher.js ~ line 2 ~ buttonStartRef', buttonStartRef);
 buttonStopRef = document.querySelector('button[data-stop]');
-console.log('🚀 ~ file: 01-color-switcher.js ~ line 4 ~ buttonStopRef', buttonStopRef);
-const intervalId = () => setInterval(onChangeBackgroundColor, 1000);
+bodyRef = document.body;
+let intervalId;
 buttonStartRef.addEventListener('click', onClickStart);
-// buttonStartRef.addEventListener('click', onClickStop);
+buttonStopRef.addEventListener('click', onClickStop);
 
-// function onClickStart(e) {
-//   e.target.setAttribute((disabled = true));
-//   setInterval(() => true);
-//   document.boby.style.backgroundColor = getRandomHexColor();
-// }
 function onClickStart(e) {
-  console.log(e.target);
-  onDisabled(e);
-  //   intervalId();
+  e.target.setAttribute('disabled', true);
+  buttonStopRef.removeAttribute('disabled');
+  intervalId = setInterval(onChangeBackgroundColor, 1000);
 }
-// function onClickStop(e) {
-//   onDisabled(e);
-//   clearInterval(intervalId);
-// }
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
-function onDisabled(e) {
-  e.target.hasAttribute('disabled')
-    ? e.target.removeAttribute('bisabled')
-    : e.target.setAttribute("disabled", true);
+function onClickStop(e) {
+  e.target.setAttribute('disabled', true);
+  buttonStartRef.removeAttribute('disabled');
+  clearInterval(intervalId);
 }
-// e.target.setAttribute(('disabled', true));
-// function onChangeBackgroundColor() {
-//   document.boby.style.backgroundColor = getRandomHexColor();
-// }
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onChangeBackgroundColor() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+}
