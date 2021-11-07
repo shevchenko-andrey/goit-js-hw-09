b881f6f59f2757aa57e344e94dc47fc37026004f
shevchenkoAndriy <s.s.student.khai.edu>
2021-11-08 01:58:39 +0200

    add feature timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,19 +1,54 @@
 import flatpickr from 'flatpickr';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import 'flatpickr/dist/flatpickr.min.css';
-const options = {
+import 'notiflix/dist/notiflix-3.1.0.min.css';
+const todayDate = Date.now();
+let selectData = null;
+let intervalId = null;
+Notify.init({
+  width: '280px',
+  position: 'right-top',
+  distance: '10px',
+  opacity: 1,
+  clickToClose: true,
+});
+const refs = {
+  datetimePicker: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('button[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+flatpickr('#datetime-picker', {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
+  onOpen() {
+    if (!refs.startBtn.hasAttribute('disabled')) {
+      refs.startBtn.setAttribute('disabled', true);
+    }
   },
-};
-const ref = {
-  datetimePicker: document.querySelector('#datetime-picker'),
-};
-flatpickr('#datetime-picker', { options });
-
+  onClose(selectedDates) {
+    if (selectedDates[0].getTime() < todayDate) {
+      Notify.failure('Please choose a date in the future');
+    } else {
+      refs.startBtn.removeAttribute('disabled');
+      selectData = selectedDates[0].getTime();
+    }
+  },
+});
+refs.startBtn.addEventListener('click', onStartTimer);
+function onStartTimer() {
+  refs.startBtn.setAttribute('disabled', true);
+  const differenceTime = calcDifferenceTime(todayDate, selectData);
+  timer(differenceTime);
+}
+function calcDifferenceTime(todayDate, selectedData) {
+  return selectedData - todayDate;
+}
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -31,4 +66,23 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
+}
+function timerMarcup({ days, hours, minutes, seconds }) {
+  refs.days.textContent = addLeadingZero(days);
+  refs.hours.textContent = addLeadingZero(hours);
+  refs.minutes.textContent = addLeadingZero(minutes);
+  refs.seconds.textContent = addLeadingZero(seconds);
+}
+function timer(selectTime) {
+  intervalId = setInterval(() => {
+    if (selectTime < 1000) {
+      refs.startBtn.removeAttribute('disabled');
+      return clearInterval(intervalId);
+    }
+    selectTime -= 1000;
+    timerMarcup(convertMs(selectTime));
+  }, 1000);
+}
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
 }
